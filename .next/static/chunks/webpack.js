c1e2520d65f4808a43d976f7a072440112a3caf7
Kevin Tran <sesouk@gmail.com>
2022-01-06 19:19:20 -0700

    connected ui

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -57,6 +57,11 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	!function() {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -131,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "850c5b8a8de7e9f4"; }
+/******/ 		__webpack_require__.h = function() { return "1c4eb95b9009df37"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
